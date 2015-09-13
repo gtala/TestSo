79ec3e14187983910a9fe8e7228eb75e02f872e3
guillermo tala <guille86598@gmail.com>
2015-09-13 14:59:27 -0300

    Probandooo

---
+++ ProjectTest2/src/ProjectTest2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ProjectTest2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!¿Que onda wacho?!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
